6a2bc44d05b81727d1f4fda2fe244892fb83e854
Hwan <dls5233@yahoo.com>
2021-09-20 22:42:47 +0900

    Create resultstartview.swift

---
+++ Catch_Frog/resultstartview.swift
@@ -0,0 +1,20 @@
+//
+//  resultstartview.swift
+//  Catch_Frog
+//
+//  Created by Hwan의 MacBook Pro on 2021/09/20.
+//
+
+import UIKit
+
+
+class resultstartview: UIViewController {
+    override func viewDidLoad() {
+        //
+    }
+    @IBAction func nametext(_ sender: UITextField) {
+    }
+    @IBAction func resultname(_ sender: UIButton) {
+    }
+    
+}
