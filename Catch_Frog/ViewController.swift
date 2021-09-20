42e0d3500ee6f3e19189ec88abe51395e33c69b5
Hwan <dls5233@yahoo.com>
2021-09-20 22:42:57 +0900

    추가

--- Catch_Frog/ViewController.swift
+++ Catch_Frog/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -16,17 +17,29 @@ class ViewController: UIViewController {
         HoleButtonArray = [Holebtn1,Holebtn2,Holebtn3,Holebtn4,Holebtn5]
         frogimage = [UIImage(named: "frog1.png")!,UIImage(named: "frog2.png")!,UIImage(named: "frog3.png")!,UIImage(named: "frog4.png")!,UIImage(named: "frog5.png")!] // frog 이미지들
 
-        while elapsedTime != 0.0 {
-            let random = arc4random() % 5
-        }
+ 
     }
     
+    
+    func start() {
+        timerstart()
+//        while elapsedTime != 0{
+//            let random = arc4random() % 5
+//            HoleButtonArray[Int(random)].setTitle("x", for: UIControl.State.normal)
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){}
+//            HoleButtonArray[Int(random)].setTitle("o", for: UIControl.State.normal)
+//        }
+//
+//        if elapsedTime == 0 {
+//            //notification 게임 종료 알림
+//        }
+    }
     
     // 게임 초기화
     func Nextgame (){
         FrogCounter = 0
         FrogCount.text = String(format: "%d",FrogCounter) // 잡은 수 초기화
-        elapsedTime = 30.0 // 남은시간 30초
+        elapsedTime = 5 // 남은시간 30초
         Time.text = String(format: "%d", elapsedTime)
     }
     
@@ -51,29 +64,50 @@ class ViewController: UIViewController {
         }
     }
     
+    // 제한 시간
+    var maintime:Timer?
+    func timerstart() {
+    maintime =  Timer.scheduledTimer(timeInterval: 1, target: self, selector: timeSelection, userInfo: nil, repeats: true) //timer,시간간격,동작될 뷰 , 타이머가 구동될 때 실행할 함수 , 사용자 정보, 반복여부
+    }
+    let timeSelection:Selector = #selector(ViewController.updateTime)
     
+    //반복실행 함수
+    @objc func updateTime(){
+        elapsedTime = elapsedTime - 1
+        Time.text = String(elapsedTime)
+        print("check 실행")
+        if elapsedTime <= 0 {
+            maintime?.invalidate()
+            maintime = nil
+        }
+    }
+    
+    //--------------------------------------------------------
+    
+    @IBAction func sv(_ sender: UIButton) {
+        start()
+    }
     
     
     
     // Object
     var HoleButtonArray: Array<UIButton> = [] //버튼 상태 저장
     var rank:Array<String> = [] // 랭크 저장용
-    var elepsedTime = 0.0 // 지나간 시간
     var FrogCounter = 0 // 잡은 개구리수
     var StartTime:Data = Data() // 현재 시간정보
-    var elapsedTime = 0.0 // 남은 시간
+    var elapsedTime = 0 // 남은 시간
     var rankscoreArray:Array<Int> = []
     var ranknameArray:Array<String> = []
     var frogimage:Array<UIImage> = []
     
     
-    //RESTART
+    //RESTART BUTTON
     @IBAction func Restart(_ sender: UIButton) {
-        if elapsedTime == 0.0 { // 게임종료여부
+        if elapsedTime == 0 { // 게임종료여부
             Nextgame()
         }else{
             // 게임중단 승인
-            //return
+            //return notification
         }
     }
     
@@ -91,7 +125,7 @@ class ViewController: UIViewController {
     
     
     
-    //MODE CHANGE
+    //MODE CHANGE BUTTON
     @IBAction func Hardmode(_ sender: UIButton) {
     }
     @IBAction func Normalmode(_ sender: UIButton) {
@@ -100,15 +134,15 @@ class ViewController: UIViewController {
     }
     
     
-    
-    //CATCH button
+    //-------------------------------------------------------
+    //CATCH BUTTON
     @IBAction func FrogHole1(_ sender: UIButton) {
         var elap = 0.0
         //game 진행여부
-        if elapsedTime != 0.0{
+        if elapsedTime != 0{
             if sender.currentTitle == "x"{ // 성공적으로 잡을 경우
                 FrogCounter += 1
-                elapsedTime += 3.0
+                elapsedTime += 3
             }else{
                 // 비활성
             }
@@ -116,14 +150,58 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func FrogHole2(_ sender: UIButton) {
+        var elap = 0.0
+        //game 진행여부
+        if elapsedTime != 0{
+            if sender.currentTitle == "x"{ // 성공적으로 잡을 경우
+                FrogCounter += 1
+                elapsedTime += 3
+            }else{
+                // 비활성
+            }
+            
+        }
     }
     @IBAction func FrogHole3(_ sender: UIButton) {
+        var elap = 0.0
+        //game 진행여부
+        if elapsedTime != 0{
+            if sender.currentTitle == "x"{ // 성공적으로 잡을 경우
+                FrogCounter += 1
+                elapsedTime += 3
+            }else{
+                // 비활성
+            }
+            
+        }
     }
     @IBAction func FrogHole4(_ sender: UIButton) {
+        var elap = 0.0
+        //game 진행여부
+        if elapsedTime != 0{
+            if sender.currentTitle == "x"{ // 성공적으로 잡을 경우
+                FrogCounter += 1
+                elapsedTime += 3
+            }else{
+                // 비활성
+            }
+            
+        }
     }
     @IBAction func FrogHole5(_ sender: UIButton) {
+        var elap = 0.0
+        //game 진행여부
+        if elapsedTime != 0{
+            if sender.currentTitle == "x"{ // 성공적으로 잡을 경우
+                FrogCounter += 1
+                elapsedTime += 3
+            }else{
+                // 비활성
+            }
+            
+        }
     }
-    
+    //-------------------------------------------------------
     
     
     //Rank Label
