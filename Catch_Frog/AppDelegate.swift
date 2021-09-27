61d99e30fd1ad4e96261d49733ba0357c4efdf83
Hwan <dls5233@yahoo.com>
2021-09-27 11:35:35 +0900

    Update AppDelegate.swift

--- Catch_Frog/AppDelegate.swift
+++ Catch_Frog/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
 
-
+    var paramname:String?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
